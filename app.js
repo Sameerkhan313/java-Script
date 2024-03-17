8baca012216604ad1fa28c5ecf2cac50ef5af295
Sameer Khan <157288172+Sameerkhan313@users.noreply.github.com>
2024-03-17 08:52:39 +0500

    changes

---
+++ app.js
@@ -0,0 +1,21 @@
+var shelf = ["pencils", "pens", "eraser", "ruler", "markers"]
+
+console.log(shelf)
+
+shelf.push("Colours")
+
+console.log(shelf)
+
+shelf.pop()
+console.log(shelf)
+
+shelf.unshift("Papers")
+console.log(shelf)
+
+shelf.shift()
+console.log(shelf)
+
+shelf.shift()
+console.log(shelf)
+
+var order1;, var order2;, var order3;,
