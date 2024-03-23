494c9ba1c37ba78385791d8c2d8ad9277265e23b
Sameer Khan <157288172+Sameerkhan313@users.noreply.github.com>
2024-03-23 15:50:35 +0500

    changes

--- app.js
+++ app.js
@@ -18,4 +18,35 @@ console.log(shelf)
 shelf.shift()
 console.log(shelf)
 
-var order1;, var order2;, var order3;,
+var order1; 
+var order2;
+var order3;
+
+// function greet (name){
+//     alert(`hello there `+ name)
+// }
+
+// greet("sameer")
+
+var shelf1 = "biryani"
+let shelf2 = "karahi"
+const shelf3 = "kheer"
+
+
+alert(shelf1)
+console.log(shelf2)
+alert(shelf3)
+
+var shelf1 = "Pulao"
+
+alert(shelf1)
+
+shelf1= "pulao"
+alert(shelf1)
+
+shelf2 = "Qorma"
+console.log (shelf2)
+
+
+
+
