ac19c1fc40c76718ad8a7b31de3c535283de180e
Sameer Khan <157288172+Sameerkhan313@users.noreply.github.com>
2024-03-24 09:00:40 +0500

    changes2

--- app.js
+++ app.js
@@ -1,52 +1,88 @@
-var shelf = ["pencils", "pens", "eraser", "ruler", "markers"]
+// var shelf = ["pencils", "pens", "eraser", "ruler", "markers"]
 
-console.log(shelf)
+// console.log(shelf)
 
-shelf.push("Colours")
+// shelf.push("Colours")
 
-console.log(shelf)
+// console.log(shelf)
 
-shelf.pop()
-console.log(shelf)
+// shelf.pop()
+// console.log(shelf)
 
-shelf.unshift("Papers")
-console.log(shelf)
+// shelf.unshift("Papers")
+// console.log(shelf)
 
-shelf.shift()
-console.log(shelf)
+// shelf.shift()
+// console.log(shelf)
 
-shelf.shift()
-console.log(shelf)
+// shelf.shift()
+// console.log(shelf)
 
-var order1; 
-var order2;
-var order3;
+// var order1; 
+// var order2;
+// var order3;
 
-// function greet (name){
-//     alert(`hello there `+ name)
-// }
+// // function greet (name){
+// //     alert(`hello there `+ name)
+// // }
 
-// greet("sameer")
+// // greet("sameer")
 
-var shelf1 = "biryani"
-let shelf2 = "karahi"
-const shelf3 = "kheer"
-
-
-alert(shelf1)
-console.log(shelf2)
-alert(shelf3)
-
-var shelf1 = "Pulao"
-
-alert(shelf1)
-
-shelf1= "pulao"
-alert(shelf1)
-
-shelf2 = "Qorma"
-console.log (shelf2)
+// var shelf1 = "biryani"
+// let shelf2 = "karahi"
+// const shelf3 = "kheer"
 
 
+// alert(shelf1)
+// console.log(shelf2)
+// alert(shelf3)
+
+// var shelf1 = "Pulao"
+
+// alert(shelf1)
+
+// shelf1= "pulao"
+// alert(shelf1)
+
+// shelf2 = "Qorma"
+// console.log (shelf2)
 
 
+// const animals = ["bandar", "hathi", "gadha","ghora"]
+// console.log(animals)
+
+// animals.push("lion")
+// console.log(animals)
+
+// animals.shift()
+// console.log(animals)
+
+// animals.unshift("cheetah")
+// console.log(animals)
+
+// animals.splice(1,2, "dog", "cat")
+// console.log(animals)
+
+// animals.splice(3,1)
+// console.log(animals)
+
+// animals.splice(3,0, "cow")
+// console.log(animals)
+
+// const petAnimals = animals.slice(1,4)
+// document.write(petAnimals)
+// document.write(animals)
+
+// console.log(petAnimals)
+// console.log(animals)
+
+restaurant prompt("apko kiya khana h?")
+alert restaurant
+
+const restaurant = ["karahi", "biryani", "qorma"]
+
+
+
+for (var i = 0; i < restaurant.length; i++){
+    alert(restaurant)
+}
